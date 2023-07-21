a9cb19fea27cd0f8651e393d6eb54db7c1dda6a6
FontanaRoman <desarrolladorautodidacta@gmail.com>
2023-07-21 16:25:58 -0300

    creacion y automatizacion de slider y carrousel de productos destacados funcionando correctamente

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,95 @@
 'use strict'
-// funcionalidad carrusel
-const carrouselSlice = document.querySelector('.carrousel__slice');
-const carrousePunto = document.querySelectorAll('.carrousel__punto');
+// nodos carruoselBanner
+const carrouselSlice = document.querySelector(".carrousel__slice");
+const carrousePunto = document.querySelectorAll(".carrousel__punto");
+const carrouselSection = document.querySelectorAll(".carrousel__section");
+// nodos menu
+const menu = document.querySelector(".header__menu-burger");
+const menuDesplegado = document.querySelector(".menu__desplegable");
 
+// funcionalidad menu-desplegable
+    menu.addEventListener("click",()=>{
+    menu.classList.toggle("active");
+    menuDesplegado.classList.toggle("desplegado");
+})
+
+// funcionalidad carrusel
+// automatizacion del carrousel para desplazarce
+setInterval(()=>{
+    slice()
+},7000);
+
+let operacion = 0;
+let contador = 0;
+let tamañoSlice = 100 / carrouselSection.length; 
+
+function slice(){
+    if(contador >= carrouselSection.length-1){
+        contador = 0;
+        operacion = 0;
+        carrouselSlice.style.transform = `translate(-${operacion}%)`;
+        return
+    }else{
+        contador++
+        operacion = operacion + tamañoSlice;
+        carrouselSlice.style.transform = `translate(-${operacion}%)`;
+        carrouselSlice.style.transition = "all ease .6s";
+    };
+}
+
+// puntos para navegar en el carrusel
 carrousePunto.forEach( ( cadaPunto , i )=> {
-    carrousePunto[i].addEventListener('click',()=>{
+    carrousePunto[i].addEventListener("click",()=>{
         let posicion  = i;
         let operacion = posicion * -33;
 
         carrouselSlice.style.transform = `translateX(${ operacion }%)`;
 
         carrousePunto.forEach( ( cadaPunto , i )=>{
-            carrousePunto[i].classList.remove('active');
+            carrousePunto[i].classList.remove("active");
         })
-        carrousePunto[i].classList.add('active');
+        carrousePunto[i].classList.add("active");
     })
 })
-console.log("que poronga este carrusel");
-
-// funcionalidad menu-desplegable
-
-const menu = document.querySelector(".header__menu-burger");
-const menuDesplegado = document.querySelector(".menu__desplegable");
 
 
-menu.addEventListener("click",()=>{
-    menu.classList.toggle("active");
-    menuDesplegado.classList.toggle("desplegado");
-})
+
+const carrouselProductsSlice = document.querySelector(".carrousel__products-slice");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const product = document.querySelectorAll(".product");
+// funcionalidad carrouselProduct
+arrowRight.addEventListener("click", e => sliceRight());
+arrowLeft.addEventListener("click", e => sliceLeft());
+
+    let operacionProducts = 0;
+    let contadorProducts = 0;
+    let tamañoSliceProducts = 100 / product.length; 
+
+function sliceRight(){
+    if(contadorProducts >= product.length-1){
+        contadorProducts = 0;
+        operacionProducts = 0;
+        carrouselProductsSlice.style.transform = `translate(-${operacionProducts}%)`;
+        return;
+    }else{
+        contadorProducts++;
+        operacionProducts = operacionProducts + tamañoSliceProducts;
+        carrouselProductsSlice.style.transform = `translate(-${operacionProducts}%)`;
+        carrouselProductsSlice.style.transition = "all ease .6s";
+    }
+}
+function sliceLeft(){
+    contadorProducts--;
+    if (contadorProducts < 0 ) {
+        contadorProducts = product.length-1;
+        operacionProducts = tamañoSliceProducts * (product.length-1)
+        carrouselProductsSlice.style.transform = `translate(-${operacionProducts}%)`;
+        carrouselProductsSlice.style.transition = "none";
+        return;
+    } else{
+        operacionProducts = operacionProducts - tamañoSliceProducts;
+        carrouselProductsSlice.style.style.transform = `translate(-${operacionProducts}%)`;
+        carrouselProductsSlice.style.style.transition = "all ease .6s"
+    }
+}
